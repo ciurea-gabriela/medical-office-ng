84394eac8c1e44b50816f25a5309f205b379ff36
gabriela-ciurea <ciurea.gabriela01@gmail.com>
2020-08-02 12:51:05 +0300

    fix project 7.0

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+const whitelist = ['https://medical-office-app.herokuapp.com']; // list of allow domain
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    if (whitelist.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}
+
+app.use(cors(corsOptions));
 
 app.use(express.static('./dist/medical-office-ng'));
 
